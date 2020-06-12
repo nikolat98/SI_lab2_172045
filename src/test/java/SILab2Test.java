9a261375eefd8abe2a52143c91e84d8a4d5a4420
Nikola Trpchevski <nikolacode9@gmail.com>
2020-06-12 16:14:38 +0200

    Lab 2

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    User user = new User("Nikola", "admin12345", "email@finki.ukim.mk");
+    SILab2 test = new SILab2();
+
+}
