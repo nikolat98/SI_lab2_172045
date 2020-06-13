c8b8660c4f05e57450abb37d0ff2f700fbd557a4
Nikola Trpchevski <nikolacode9@gmail.com>
2020-06-13 13:26:08 +0200

    added everyPath

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,27 +9,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SILab2Test {
 
-    private List<String> createList(String usernames) {
-        List<String> users = new ArrayList<>();
-        return users;
+    private List<String> initList(String... usernames) {
+        return new ArrayList<>(Arrays.asList(usernames));
     }
     SILab2 testunit = new SILab2();
 
+
+
+
     @Test
     void everyPathTest() {
-        // prva pateka A,K
-        assertFalse(testunit.function(null, null));
+        // A-K user==null
+        assertEquals(false, testunit.function(null, initList("nikola")));
+        //A-B-K
+        User user1 = new User(null, "12345", "nikola@gmail.com");
+        assertEquals(false, testunit.function(user1, initList("filip")));
 
-        // A,B,K
-        assertFalse(testunit.function(new User(null, null, null), null));
+        // ako mailot e null
+        User user2 = new User("nikola", "12345", null);
+        assertEquals(false, testunit.function(user2, initList("random")));
 
-        // A,B,C,D1,D2,I,J
-        assertFalse(testunit.function(new User("nikola", "123123", ""), createList("username")));
-        // A,B,C,D1,D2,I,K
-        
-        //assertFalse(testunit.function(new User("n1kola", "12345", ""), generated));
-        // A,B,C,D1,(D2,E,F,G,H,D3,D2),I,J
-        // A,B,C,D1,(D2,E,G,H,D3,D2),I,J
+        // A,B,C,D1,D2,I,K (opfatena od a-b-k)
+
+        // A,B,C,D1,D2,(E,F,G,H,D3,D2),I,J
+        User user3 = new User("nikola", "12345", "nikola@gmail.com");
+        assertEquals(true, testunit.function(user3, initList("random")));
+
+    }
+
+    @Test
+    void everyBranchTest() {
+        // prv branch A->K if(user == null)
+        assertEquals(false, testunit.function(null, initList("nikola")));
+
+        // A->B (expected true) ->
+        User user1 = new User("nikola", "pass123", "nikola@gmail.com");
+        assertEquals(true, testunit.function(user1, initList("goce")));
+
+        // B->K expected false ako barem eden e null od uslovite
+        User falseUser = new User(null, "asdasda", "random@finki.mk");
+        assertEquals(false, testunit.function(falseUser, initList("random1", "random2")));
+
+
+        // B->C->D1->D2
+        User user2 = new User("nikola", "pass123", null);
+        assertEquals(false, testunit.function(user2, initList("randomUsername")));
+
+        // D1->D2->(E->F->G->H->D3->D2)->I->J ocekuva true
+        User user3 = new User("goce", "snagata", "random@yahoo.com");
+        assertEquals(true, testunit.function(user3, initList("c1c2c3", "javascript", "python")));
 
 
     }
